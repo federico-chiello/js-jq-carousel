11385b9556da24916f76295618886b012d5a9484
Fede <chiellofederico@gmail.com>
2021-01-08 15:24:43 +0100

    init jquery

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,19 @@
 // Istruzione:
 // Lo slider prevederà due frecce che permettono di scorrere tra le immagini dello slider, sia con la freccia di destra che con quella di sinistra.
 // In oltre, per scorrere le immagini, attivare anche l’uso delle frecce sinistra e destra della tastiera.
+
+
+$(document).ready(function(){
+  // Creiamo un evento al click. Quando clicchiamo la freccia di destra devono scorrere le immagini.
+  $('.next').click(function(){
+    // Utilizziamo una variabile per indicare la condizione iniziale dell'active, il quale si dovrà spostare nelle immagini successive al click.
+    var immagini = $('.images img.active');
+
+
+
+  });
+
+
+
+
+});

206be40ea1cda3721236ec604e410ae9ab88ff93
Fede <chiellofederico@gmail.com>
2021-01-08 15:09:00 +0100

    Init

---
+++ js/myscript.js
@@ -0,0 +1,3 @@
+// Istruzione:
+// Lo slider prevederà due frecce che permettono di scorrere tra le immagini dello slider, sia con la freccia di destra che con quella di sinistra.
+// In oltre, per scorrere le immagini, attivare anche l’uso delle frecce sinistra e destra della tastiera.
